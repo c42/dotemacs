916938aefaa1d0d4f2b1fbbbb2708b01955ad425
Akshay Gupta <akshay@c42.in>
2012-06-06 13:06:08 +0530

    [sd/k] js offset

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(rspec-use-rake-flag nil))
+ '(js2-basic-offset 2)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
