3e96af09332d316f2348773d8476067fe9f3ef50
Steven Deobald <steven@c42.in>
2012-06-23 17:00:16 +0530

    [sd] adding ctags with auto-reloading. yeah, dawg. meta-dot in da houz.

--- steven.el
+++ steven.el
@@ -3,3 +3,10 @@
 (set-face-attribute 'default nil
                     :family "Anonymous Pro"
                     :height 160)
+
+(require 'find-file-in-project)
+(defun open-project (dir)
+  (interactive "DDirector: ")
+  (cd dir)
+  (ffip-project-root))
+
